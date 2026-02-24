10ae2ae8e153fcefbedf4110711113496e462047
Shashank Shekhar <shashank.s@crowdanalytix.com>
2026-02-24 21:30:17 +0530

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Fraunces, Manrope } from "next/font/google";
 import "./globals.css";
 import "../styles/custom.scss";
@@ -42,6 +43,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${body.variable} ${display.variable}`}>
       <body className="bg-ink-900 text-sand-100 antialiased">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4KL2L6RSXJ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4KL2L6RSXJ');
+          `}
+        </Script>
         {children}
       </body>
     </html>
